5aa2f59ef9e6b385250f1eeb9f48b1156029ae7b
WR540 <wilsoncheng889@gmail.com>
2025-02-14 11:04:10 +0800

    Wilson - testing compatibility

--- Github Test1.cpp
+++ Github Test1.cpp
@@ -1,9 +1,16 @@
 
 #include <iostream>
+#include "Windows.h"
 
 int main()
 {
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, 0x0A);
+
     std::cout << "Land of the rising Sun\n";
     std::cout << "Main 1 test - Ainsley\n";
+
+    std::cout << "Test 2 - Wilson" << std::endl;
+    
 }
 
